c646132b4ec9747dd836ab30a7f2c54004455bcd
Mark Adreian Ramos <121351652+markRamos1974@users.noreply.github.com>
2024-01-26 18:59:31 +0800

    setting up development environment

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// const path = document.querySelector('path')
+// const pathLength = path.getTotalLength()
+
+// console.log(pathLength)
+
+// path.style['stroke-dasharray'] = pathLength;
+// path.style['stroke-dashoffset']  = pathLength;
+// path.style['transition'] = '5000ms ease-in-out'
+
+// window.addEventListener('scroll', () => {
+//     // console.log("Document element: " + document.documentElement.scrollTop)
+//     // console.log("Document body: " + document.body.scrollTop)
+//     // console.log("Scroll height: " + document.documentElement.scrollHeight)
+//     // console.log("Client height: " + document.documentElement.clientHeight)
+
+//     let scrollPercentage = (document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+//     let drawLength = pathLength;
+//     path.style.strokeDashoffset = pathLength - drawLength
+// })  
