a216411fdd3b7fa6aacbe8c752e0cd5b4b224e3b
Mark Adreian Ramos <121351652+markRamos1974@users.noreply.github.com>
2024-01-28 17:35:53 +0800

    adding responsive layout to about section

--- js/index.js
+++ js/index.js
@@ -2,35 +2,79 @@
 const webfolio = document.querySelector(".webfolio")
 const header = document.querySelector(".header")
 
+
 window.addEventListener("scroll", () => {
-    let webfolioHeight = webfolio.getClientRects()[0].height;
+    let screenWitdth = document.documentElement.clientWidth
+    let webfolioHeight = webfolio.getClientRects()[0].height
     let scrollValue = document.documentElement.scrollTop 
- 
+
+    /*
+        Adding padding in px for specific screen sizes to prevent sudden clip from
+        parrallax scrolling for header
+    */
+    if(screenWitdth < 768) webfolioHeight += 16             //Mobile
+    else if (screenWitdth < 1280) webfolioHeight += 32      //Tablet
+    else webfolioHeight += 64                               //Desktop from 1280px to 1920px and up
+    
+
+    // Fixing header to top when after a specific scroll value when scrolling
     if(scrollValue >= webfolioHeight){
+
+        /* 
+
+            preventing main content section from sudden clip
+            when scrolled to a specific value
+
+        */ 
+        webfolio.style.marginBottom = "calc(24px + 12px)" 
+
+        //Fixing header to top of the page
         header.style.position = "fixed"
         header.style.top = "0"
        
-        if (document.documentElement.clientWidth < 768) header.style.width = "calc(100% - 2rem)"
-        else if (document.documentElement.clientWidth < 1280)  header.style.width = "calc(100% - 4rem)"
-        else  header.style.width = "calc(100% - 8rem)"
+        /*
+            Setting header with for specific screen 
+            sizes when fixed to the top of the page
+        */
+
+        // Mobile view
+        if (document.documentElement.clientWidth < 768) 
+            header.style.width = "calc(100% - 2rem)"
+        // Tablet view
+        else if (document.documentElement.clientWidth < 1280)  
+            header.style.width = "calc(100% - 4rem)"
+        // Desktop view
+        else  
+            header.style.width = "calc(100% - 4rem)"
 
         
     }
     else {
+        //returns to default state then scrolled back to the top of the page
+        webfolio.style.marginBottom = "0px"
         header.style.position = "static"
         header.style.width = "100%"
     }
   
 })
 
+// Adapting header to screen resize when scrolling and header is fixed on top
 window.addEventListener('resize', () => {
     let webfolioHeight = webfolio.getClientRects()[0].height;
     let scrollValue = document.documentElement.scrollTop 
- 
+    
+
     if(scrollValue >= webfolioHeight){
-        if (document.documentElement.clientWidth < 768) header.style.width = "calc(100% - 2rem)"
-        else if (document.documentElement.clientWidth < 1280)  header.style.width = "calc(100% - 4rem)"
-        else  header.style.width = "calc(100% - 8rem)"
+
+        // Mobile view
+        if (document.documentElement.clientWidth < 768) 
+            header.style.width = "calc(100% - 2rem)"
+        // Tablet view
+        else if (document.documentElement.clientWidth < 1280)  
+            header.style.width = "calc(100% - 4rem)"
+        // Desktop
+        else  
+            header.style.width = "calc(100% - 4rem)"
     }
 })
 
@@ -45,22 +89,21 @@ window.addEventListener('resize', () => {
 
 
 
-// const path = document.querySelector('path')
-// const pathLength = path.getTotalLength()
+const path = document.querySelector('path')
+const pathLength = path.getTotalLength()
 
-// console.log(pathLength)
+console.log(path)
+path.style.strokeDasharray = pathLength;
+path.style.strokeDashoffset  = pathLength;
+path.style.transition = '5000ms ease-in-out'
 
-// path.style['stroke-dasharray'] = pathLength;
-// path.style['stroke-dashoffset']  = pathLength;
-// path.style['transition'] = '5000ms ease-in-out'
+window.addEventListener('scroll', () => {
+    // console.log("Document element: " + document.documentElement.scrollTop)
+    // console.log("Document body: " + document.body.scrollTop)
+    // console.log("Scroll height: " + document.documentElement.scrollHeight)
+    // console.log("Client height: " + document.documentElement.clientHeight)
 
-// window.addEventListener('scroll', () => {
-//     // console.log("Document element: " + document.documentElement.scrollTop)
-//     // console.log("Document body: " + document.body.scrollTop)
-//     // console.log("Scroll height: " + document.documentElement.scrollHeight)
-//     // console.log("Client height: " + document.documentElement.clientHeight)
-
-//     let scrollPercentage = (document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-//     let drawLength = pathLength;
-//     path.style.strokeDashoffset = pathLength - drawLength
-// })  
+    let scrollPercentage = (document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    let drawLength = pathLength;
+    path.style.strokeDashoffset = pathLength - drawLength
+})  
