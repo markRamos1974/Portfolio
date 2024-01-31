2f0fdfc850a09382a0781cafe4e4c0283ee2eee1
Mark Adreian Ramos <121351652+markRamos1974@users.noreply.github.com>
2024-01-31 18:22:31 +0800

    finalizing scrool interaction for the project section

--- js/projectSection.js
+++ js/projectSection.js
@@ -16,121 +16,9 @@ else {
     setCarouselToMobileView(670);
 }
 
-
-
-
-
-
-//Carousel Section
-const projectList = document.querySelector('.project-carousel > ul')
-const projectItems = projectList.querySelectorAll("li")
-const projectTracker = document.querySelector('.project-tracker')
-const arrowLeft = document.querySelector(".arrow-left")
-const arrowRight = document.querySelector(".arrow-right") 
-console.log(arrowLeft)
-console.log(arrowRight)
-projectList.style.transition = "400ms ease-in-out"
-arrowLeft.style.stroke = "var(--dark-blue)";
-arrowRight.style.stroke = "var(--white)";
-let currentPosition = 0;
-let projectNumber = 1
-let mousePostionStart = 0;
-let mousePostionEnd  = 0;
-let pressed = false
-
-const moveListBy = amount => {
-   
-    if (currentPosition == 0 && amount > 0 || currentPosition == -(document.documentElement.clientWidth * 3) && amount < 0) return
-    else {
-        currentPosition += amount
-        projectList.style.transform = `translateX(${currentPosition}px)` 
-    }
-    projectTracker.innerHTML = `0${projectNumber} / 04`
-    
-}
-projectList.addEventListener("mousedown", (e) => {
-    pressed = true
-    mousePostionStart = e.clientX; 
-  
-})
-projectList.addEventListener("mouseup", (e) => {
-    let currentMousePosition = e.clientX;
-    if(!pressed){
-        return
-    }
-    else {
-        let direction = currentMousePosition - mousePostionStart; 
-        
-        if (direction < 0 ){
-            projectNumber += 1
-            moveListBy(-document.documentElement.clientWidth)
-            
-            
-        }
-        else{
-            projectNumber--
-            moveListBy(document.documentElement.clientWidth)
-            
-           
-        }   
-    }
-    pressed = false
-})
-
-
-
-arrowLeft.addEventListener("click", () => {
-    arrowRight.style.stroke = "var(--white)"
-    
-    if(projectNumber == 1){
-        arrowLeft.style.stroke = "var(--dark-blue)";
-        return
-    }
-    else if(projectNumber == 2){
-        projectNumber--
-        arrowLeft.style.stroke = "var(--dark-blue)";
-        moveListBy(document.documentElement.clientWidth)
-    }
-    else {
-        projectNumber--
-        console.log(projectNumber)
-        arrowLeft.style.stroke = "var(--white)";
-        moveListBy(document.documentElement.clientWidth)
-    }
-    
-})
-arrowRight.addEventListener("click", () => {
-
-        if(projectNumber >= 4) {
-            arrowRight.style.stroke = "var(--dark-blue)"
-            return
-        }else {
-            projectNumber++
-            if(projectNumber == 4) arrowRight.style.stroke = "var(--dark-blue)"
-            arrowLeft.style.stroke = "var(--white)";
-            moveListBy(-document.documentElement.clientWidth)
-        }
-        
-    
-    
-})
-
-
-
-
-const resetProjectList = () => {
-    currentPosition = 0
-    projectNumber = 1
-    projectList.style.transform = `translateX(0px)`
-    projectTracker.innerHTML =  `0${projectNumber} / 04`
-
-}
-
-
 window.addEventListener('resize', () => {
     if(document.documentElement.clientWidth < 768){
         setCarouselToMobileView(117);
-        resetProjectList()
     }
     else if (document.documentElement.clientWidth < 1280){
         setCarouselToMobileView(570);
@@ -138,18 +26,84 @@ window.addEventListener('resize', () => {
     else {
         setCarouselToMobileView(670);
     }
-
-    if(document.documentElement.clientWidth >= 768){
-        resetProjectList()
-    }
-
-    
-
 })
 
 
+//Carousel Section
+const slider = document.querySelector(".project-carousel > ul")
+const projectTracker = document.querySelector('.project-tracker')
+const arrowLeft = document.querySelector('.arrow-left')
+const arrowRight = document.querySelector('.arrow-right')
+
+let startX;
+let velX;
+let scrollLeft;
+let projectNumber = 1;
+let momentumID;
+
+let isMouseDown = false
+
+slider.addEventListener("mousedown", (e)=> {
+    cancelMomentumTracking()
+    beginSliding(e)
+}) 
+
+slider.addEventListener("mouseleave", (e) => {
+    beginMomentumTracking()
+    if(!isMouseDown) return
+})
+
+slider.addEventListener("mouseup", (e) => {
+    stopSliding(e)
+    beginMomentumTracking()
+})
+
+slider.addEventListener("mousemove", (e) => {
+    
+    slide(e)
+})
 
 
+const beginSliding = (e) => {
+    isMouseDown = true
+    startX = e.pageX - slider.offsetLeft
+    scrollLeft = slider.scrollLeft
+}
+
+const stopSliding = (e) => {
+    
+    isMouseDown = false
+    return
+}
+
+const slide = (e) => {
+    if(!isMouseDown) return
+    e.preventDefault()
+    
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX); //scroll-fast
+    var prevScrollLeft = slider.scrollLeft
+    slider.scrollLeft = (scrollLeft - walk)
+    velX = slider.scrollLeft - prevScrollLeft; 
+   
+    
+}
 
 
- 
+function beginMomentumTracking(){
+    cancelMomentumTracking();
+    momentumID = requestAnimationFrame(momentumLoop);
+}
+function cancelMomentumTracking(){
+    cancelAnimationFrame(momentumID);
+}
+function momentumLoop(){
+    slider.scrollLeft += velX;
+    velX *= 0.95; 
+    if (Math.abs(velX) > 0.5){
+        momentumID = requestAnimationFrame(momentumLoop);
+    }
+
+    projectNumber =  Math.round(slider.scrollLeft / document.documentElement.clientWidth) + 1
+    projectTracker.innerHTML = `0${projectNumber} / 04`
+}
